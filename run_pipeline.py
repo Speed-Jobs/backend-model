c6a6cb18f3dbe8d0fba94f57b5ce87474d1a4ddb
UiJin_Lee <mr938363@gmail.com>
2025-12-19 09:35:56 +0900

    cleanup files

--- run_pipeline.py
+++
@@ -1,9 +0,0 @@
-"""
-Wrapper script to run the data pipeline
-
-This script imports and executes the main pipeline from app/core/pipeline
-"""
-from app.core.pipeline.run_data_pipeline import main
-
-if __name__ == "__main__":
-    main()
